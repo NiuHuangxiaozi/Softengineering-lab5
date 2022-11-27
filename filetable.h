1e7136e3f8b0e11985e5c980b20d371df6cf2fa4
NiuHuangxiaozi <1280773543@qq.com>
2022-11-27 21:36:29 +0800

    Feature 1:完成了整个IO操作过程，实现了文件的输入输出

---
+++ filetable.h
@@ -0,0 +1,62 @@
+#ifndef FILETABLE_H
+#define FILETABLE_H
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <QDebug>
+class File
+{
+private:
+    int is_equal;
+    std::string url1;
+    std::string url2;
+public:
+    File(std::string new_url1, std::string new_url2)
+    {
+        url1 = new_url1;
+        url2 = new_url2;
+    }
+    void set_result(int flag);
+    int Get_result(){return is_equal;}
+    std::vector<std::string> Getpair();
+};
+
+
+
+class FileTable
+{
+public:
+    virtual void Get_file_pairs(std::string& s1, std::string& s2,int index) {};
+    //virtual void Add_file_pairs(std::string& s1, std::string& s2) {};
+    //virtual bool is_empty() {return true;};
+    virtual void save_file_result(std::string s) {};
+    virtual void init_data(std::string s,std::string answerfile) {};
+    virtual void Set_answer(int answer){};
+    virtual int Get_index(){return 1;};
+    virtual bool is_over(){return true;};
+    virtual void reset(){};//恢复原态
+    virtual int get_file_nums(){return 0;};
+    virtual void set_next_index(){};
+
+};
+class LinearFileTable:public FileTable
+{
+
+private:
+    std::vector<File> files;//文件的数据结构
+    int index=0;//目前在哪一个文件对
+public:
+    void init_data(std::string eqpath,std::string uneqpath);
+    void Get_file_pairs(std::string& s1, std::string& s2,int index);
+    void Set_answer(int answer);
+    int Get_index();
+    bool is_over();
+    void reset();//恢复原态
+    int get_file_nums();
+    void save_file_result(std::string s);
+    void set_next_index();
+
+
+};
+#endif // FILETABLE_H
